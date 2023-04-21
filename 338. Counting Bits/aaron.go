e815e5f86ba88463ff6b68b1d77b8cf55b0136c2
hsingyingli <xingyingli855@gmail.com>
2023-04-21 21:38:11 +0800

    aaron:338

---
+++ 338. Counting Bits/aaron.go
@@ -0,0 +1,11 @@
+func countBits(n int) []int {
+	dp := make([]int, n+1)
+	offset := 1
+	for i := 1; i <= n; i++ {
+		if offset*2 == i {
+			offset *= 2
+		}
+		dp[i] = 1 + dp[i-offset]
+	}
+	return dp
+}
